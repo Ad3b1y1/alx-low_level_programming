750767189a9f124733c70bf1ea961a1b6cd9d031
Ad3b1y1 <opeoluwaadebiyi@outlook.com>
2022-09-09 03:57:20 +0000

    task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+  printf("with proper grammer, but the outcome is a piece is a place of art,\n");
+  return (0);
+}
