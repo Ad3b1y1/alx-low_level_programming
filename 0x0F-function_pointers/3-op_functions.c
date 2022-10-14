7fd884282f1e9d41f58491c78c50fe7cfbdc9637
Ad3b1y1 <opeoluwaadebiyi@outlook.com>
2022-10-14 17:41:03 +0000

    A goal is not always meant to be reached

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,12 +1,12 @@
 #include "3-calc.h"
 
-/**
- * op_add - adds two numbers.
- * @a: first number.
- * @b: second number.
- *
- * Return: add.
- */
+ /**
+  * op_add - adds two numbers.
+  * @a: first number.
+  * @b: second number.
+  *
+  * Return: add.
+  */
 int op_add(int a, int b)
 {
 return (a + b);
